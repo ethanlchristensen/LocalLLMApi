ab66f33509770e00a4685ab1d9284071c6390bfc
ethanlchristensen <ethanlchristensen@outlook.com>
2024-07-12 23:59:21 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+run 
 # Local LLM API
 This is a simple API to faciliate making API calls to various locally hosted / other LLM technologies.
 - Image generation
--- testing/utils.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def save_image(path: str, data: str):
-    with open(
-        path,
-        "wb",
-    ) as image_file:
-        image_file.write(base64.b64decode(data))
-        
